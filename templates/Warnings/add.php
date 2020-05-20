f7e3e745516900e51eaea72044b2fb81c6cb313b
thanhch <thanhch@gamota.com>
2020-05-20 17:57:01 +0700

    create project cake php 4 use plugin cake DC--- login success

---
+++ templates/Warnings/add.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Warning $warning
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Warnings'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="warnings form content">
+            <?= $this->Form->create($warning) ?>
+            <fieldset>
+                <legend><?= __('Add Warning') ?></legend>
+                <?php
+                    echo $this->Form->control('name');
+                    echo $this->Form->control('description');
+                    echo $this->Form->control('source', [
+                        'options' => ['telegram' => 'Telegram', 'email' => 'Email']]);
+                    echo $this->Form->control('telegram');
+                    echo $this->Form->control('email_list');
+                    echo $this->Form->control('time');
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
